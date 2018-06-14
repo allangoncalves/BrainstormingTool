49aad4015c45ec3ec92d1b741a0b9b74434c8810
Allan Gonçalves <allangoncalves@outlook.com>
2018-06-14 19:23:49 -0300

    documentando os testes

--- src/test/java/bp/IdeaTest.java
+++ src/test/java/bp/IdeaTest.java
@@ -1,65 +1,83 @@
 package bp;
 
 import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.assertNotNull;
 import org.junit.Before;
 import org.junit.Test;
 
 public class IdeaTest {
-	
-	public User owner;
-	public Idea idea;
-	public User participant;
-	public Session session;
-	
-	@Before
-	public void setUp() {
-		this.owner = new User("Amano Kun");
-		this.session = new Session(this.owner);
-		this.idea = new Idea(this.owner, "breve descricao");
-		this.idea.session = this.session;
-		
-	}
-	
-	@Test
-	public void createIdeaTest() {
-		assertEquals(this.owner, this.idea.author);
-		assertEquals(0, this.idea.voters.size());
-	}
-	
-	@Test
-	public void ideaVoteTest() {
-		this.idea.session.phase = SessionPhase.BRAINSTORM;
-		this.idea.registerVote(new User("Zorome"));
-		assertEquals(0, this.idea.voters.size());
-		
-		
-		this.idea.session.phase = SessionPhase.VOTING;
-		this.idea.registerVote(this.participant);
-		assertEquals(0, this.idea.voters.size());
-		
-		this.idea.registerVote(this.owner);
-		assertEquals(0, this.idea.voters.size());
-		
-		
-		this.participant.votes = 10;
-		this.idea.session.addParticipant(this.participant);
-		this.idea.registerVote(this.participant);
-		assertEquals(0, this.idea.voters.size());
-		
-	}
-	
-	@Test
-	public void ideaUnvoteTest() {
-		this.idea.session.phase = SessionPhase.VOTING;
-		this.idea.session.addParticipant(this.participant);
-		this.idea.registerVote(this.participant);
-		this.idea.session.phase = SessionPhase.RANK;
-		this.idea.reclaimVote(this.participant);
-		assertEquals(1, this.idea.voters.size());
-		
-		this.idea.reclaimVote(new User("Zero Two"));
-		assertEquals(1, this.idea.voters.size());
-	}
+
+  public User owner;
+  public Idea idea;
+  public User randomUser;
+  public Session session;
+
+  @Before
+  public void setUp() {
+    this.owner = new User("Amano Kun");
+    this.session = new Session(this.owner);
+    this.idea = new Idea(this.owner, "breve descricao");
+    this.idea.session = this.session;
+    this.randomUser = new User("Participant");
+  }
+
+  @Test
+  public void createIdeaTest() {
+    //O autor da ideia deve ser o mesmo que foi passado na sua criação
+    assertEquals(this.owner, this.idea.author);
+    
+    //A lista de ideias não pode ser nula
+    assertNotNull(this.idea.voters);
+    
+    // A lista de votantes deve estar vazia
+    assertEquals(0, this.idea.voters.size());
+  }
+
+  @Test
+  public void ideaVoteTest() {
+    //Os votos permanecem inalterados se a sessão não estiver na fase de votação
+    this.idea.session.phase = SessionPhase.BRAINSTORM;
+    this.idea.registerVote(new User("Zorome"));
+    assertEquals(0, this.idea.voters.size());
+
+    //Os votos permanecem inalterados se o votante não estiver participando da sessão
+    this.idea.session.phase = SessionPhase.VOTING;
+    this.idea.registerVote(this.randomUser);
+    assertEquals(0, this.idea.voters.size());
+
+    //Permanece alterada se o votante for o autor da ideia
+    this.idea.registerVote(this.owner);
+    assertEquals(0, this.idea.voters.size());
+
+    //Os votos permanecem inalterados se o votante já tiver alcançado o limite de votos
+    this.idea.session.addParticipant(this.randomUser);
+    this.randomUser.votes = 3;
+    this.idea.registerVote(this.randomUser);
+    assertEquals(0, this.idea.voters.size());
+
+  }
+
+  @Test
+  public void ideaUnvoteTest() {
+    //Voto válido
+    this.idea.session.phase = SessionPhase.VOTING;
+    this.idea.session.addParticipant(this.randomUser);
+    this.idea.registerVote(this.randomUser);
+    assertEquals(1, this.idea.voters.size());
+    
+    //Os votos permanecem inalterados se a sessão não estiver na fase de votação
+    this.idea.session.phase = SessionPhase.RANK;
+    this.idea.reclaimVote(this.randomUser);
+    assertEquals(1, this.idea.voters.size());
+
+    //Quem quer remover o voto deixa de fazer parte da lista de votantes e o tamanho é decrementado
+    this.idea.session.phase = SessionPhase.VOTING;
+    this.idea.reclaimVote(this.randomUser);
+    assertEquals(0, this.idea.voters.size());
+    
+    //Os votos permanecem inalterados se quem quer remover o voto não esta na lista de participantes
+    this.idea.reclaimVote(new User("Zero Two"));
+    assertEquals(0, this.idea.voters.size());
+  }
 
 }
--- src/test/java/bp/SessionTest.java
+++ src/test/java/bp/SessionTest.java
@@ -11,81 +11,102 @@ import org.junit.Test;
  */
 
 public class SessionTest {
-	
-	public Session session;
-	public User user;
-	
-	@Before
-	public void setUp() {
-		this.user = new User("John Doe");
-		this.session = new Session(this.user);
-		
-	}
-	
-	@Test
-	public void constructorTest() {
-		assertEquals(this.user, this.session.owner);
-		assertEquals(SessionPhase.WELCOME, this.session.phase);
-		assertNotNull(this.session.ideas);
-		assertNotNull(this.session.participants);
-		assertEquals(0, this.session.ideas.size());
-		assertEquals(0, this.session.participants.size());
-	}
-	
-	@Test
-	public void nextPhase() {
-		this.session.phase = SessionPhase.WELCOME;
-		assertEquals(this.session.nextPhase(), SessionPhase.BRAINSTORM);
-		
-		this.session.phase = SessionPhase.BRAINSTORM;
-		assertEquals(this.session.nextPhase(), SessionPhase.VOTING);
-		
-		this.session.phase = SessionPhase.VOTING;
-		assertEquals(this.session.nextPhase(), SessionPhase.RANK);
-		
-		this.session.phase = SessionPhase.RANK;
-		assertEquals(this.session.nextPhase(), SessionPhase.RANK);
-	}
-	 
-	@Test
-	public void newIdeaTest() {
-		this.session.phase = SessionPhase.WELCOME;
-		this.session.addIdea(new Idea(this.user, "Description"));
-		assertEquals(0, session.ideas.size());
-		
-		this.session.phase = SessionPhase.BRAINSTORM;
-		this.session.addIdea(new Idea(new User(""), "Description"));
-		assertEquals(0, this.session.ideas.size());
-		
-		this.session.addIdea(new Idea(this.user, "Description"));
-		assertEquals(1, this.session.ideas.size());
-	}
-	
-	@Test
-	public void addIdeaTest() {
-		this.session.phase = SessionPhase.BRAINSTORM;
-		this.session.addParticipant(this.user);
-		this.session.addIdea(new Idea(this.user, "Description"));
-		assertEquals(1, this.session.ideas.size());
-	}
-	
-	@Test
-	public void addParticipantTest() {
-		this.session.phase = SessionPhase.BRAINSTORM;
-		this.session.addParticipant(new User("Cicrano"));
-		assertEquals(1, this.session.participants.size());
-		
-		this.session.addParticipant(this.user);
-		assertEquals(1, this.session.participants.size());
-	}
-	
-	@Test
-	public void removeParticipantTest() {
-		this.session.removeParticipant(new User(""));
-		assertEquals(1, this.session.participants.size());
-		
-		this.session.removeParticipant(this.user);
-		assertEquals(0, this.session.participants.size());
-	}
+
+  public Session session;
+  public User user;
+
+  @Before
+  public void setUp() {
+    this.user = new User("John Doe");
+    this.session = new Session(this.user);
+
+  }
+
+  @Test
+  public void constructorTest() {
+    //O proprietário deve ser o mesmo do que foi passado na sua construção
+    assertEquals(this.user, this.session.owner);
+
+    //Deve ser inicializada com a fase WELCOME
+    assertEquals(SessionPhase.WELCOME, this.session.phase);
+
+    //A lista não pode ser nula
+    assertNotNull(this.session.ideas);
+
+    //A lista não pode ser nula
+    assertNotNull(this.session.participants);
+
+    //A lista tem que estar vazia
+    assertEquals(0, this.session.ideas.size());
+
+    //A lista tem que estar vazia
+    assertEquals(0, this.session.participants.size());
+  }
+
+  @Test
+  public void nextPhase() {
+
+    //Se tiver na fase de acolhimento, a sessão passa para a fase de Brainstorming
+    this.session.phase = SessionPhase.WELCOME;
+    assertEquals(this.session.nextPhase(), SessionPhase.BRAINSTORM);
+
+    //Se tiver na fase de Brainstorming, a sessão passa para a fase de votação
+    this.session.phase = SessionPhase.BRAINSTORM;
+    assertEquals(this.session.nextPhase(), SessionPhase.VOTING);
+
+    //Se tiver na fase de votação, a sessão passa para a fase de encerramento
+    this.session.phase = SessionPhase.VOTING;
+    assertEquals(this.session.nextPhase(), SessionPhase.RANK);
+
+    //Se tiver na fase de encerramento, nada acontece
+    this.session.phase = SessionPhase.RANK;
+    assertEquals(this.session.nextPhase(), SessionPhase.RANK);
+  }
+
+  @Test
+  public void newIdeaTest() {
+    //A lista de ideias é inalterada se a sessão não estiver na fase de brainstorming
+    this.session.phase = SessionPhase.WELCOME;
+    this.session.addIdea(new Idea(this.user, "Description"));
+    assertEquals(0, session.ideas.size());
+    
+    //A lista de ideias é inelterada se o autor da ideia não for um participante da sessao
+    this.session.phase = SessionPhase.BRAINSTORM;
+    this.session.addIdea(new Idea(new User(""), "Description"));
+    assertEquals(0, this.session.ideas.size());
+
+    //caso valido, a ideia é adicionada
+    this.session.addIdea(new Idea(this.user, "Description"));
+    assertEquals(1, this.session.ideas.size());
+  }
+
+  @Test
+  public void addParticipantTest() {
+    //A lista é inalterada se não estiver na fase de acolhimento
+    this.session.phase = SessionPhase.BRAINSTORM;
+    this.session.addParticipant(new User("Jane Doe"));
+    assertEquals(1, this.session.participants.size());
+
+    //A lista de participantes é inalterada se o mesmo usuário for adicionado
+    this.session.addParticipant(this.user);
+    assertEquals(1, this.session.participants.size());
+    
+    //Participante adicionado na fase de acolhimento deve ser aceito
+    this.session.phase = SessionPhase.WELCOME;
+    this.session.addParticipant(new User("Jane Doe"));
+    assertEquals(2, this.session.participants.size());
+    
+  }
+
+  @Test
+  public void removeParticipantTest() {
+    //A lista de participantes é inalterada se o usuário não é um participante
+    this.session.removeParticipant(new User(""));
+    assertEquals(1, this.session.participants.size());
+
+    //Se o usuário for um participante, ele deve ser removido da lista de participantes
+    this.session.removeParticipant(this.user);
+    assertEquals(0, this.session.participants.size());
+  }
 
 }
